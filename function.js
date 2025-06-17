66d784df35ed0d6e545895790c0c264f37eb60ed
itsniranjannn <niranjankatwal33@gmail.com>
2025-06-17 18:11:56 +0545

    starting function

---
+++ function.js
@@ -0,0 +1,7 @@
+//---------------------FUNCTION--------------------//
+/*function functionname(parameters){
+    condition.......................
+}
+functionname->this is refrence ()-> this is for executing
+
+functionname(arguments)  -> this is full execution of function*/
