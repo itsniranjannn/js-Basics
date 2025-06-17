9caf03aec7e9f0486e2c95809c556b20e34bdcef
itsniranjannn <niranjankatwal33@gmail.com>
2025-06-17 21:40:48 +0545

    fbasic

--- function.js
+++ function.js
@@ -5,3 +5,14 @@
 functionname->this is refrence ()-> this is for executing
 
 functionname(arguments)  -> this is full execution of function*/
+/*function fullName(){
+    let naame = "Niraanjan"
+    let caste = "katwal"
+   
+    return naame + " "+caste;       // here return stops execution and returs the result 
+}
+console.log(fullName()) */ //this print the function result after execution
+/*function add(x, y){
+    console.log("add= ", x + y);
+    }// we can get output by this method also 
+    add(20, 45)//without return directly calling function */
