9785ec2a214923b0c7959bb8cf8c06265d4a6262
itsniranjannn <niranjankatwal33@gmail.com>
2025-06-25 18:19:29 +0545

    ob done

--- object.js
+++ object.js
@@ -34,7 +34,7 @@ console.log(arr);
 const onnj = new Object ({name: "niranjan", age : 22,})
 console.log(typeof onnj)*/
 
-// OBJECT CONSTRUCTOR
+/*// OBJECT CONSTRUCTOR
 function Person(first, last, age, eye) {
 // this  is constructor we need to define first in constructor to add in object, if need to add any thing add above after age, nation
   this.firstName = first; 
@@ -62,11 +62,12 @@ console.log(me.fullName())
 //     }
 //     const meroCar = new car ("TATA", 200);
 
-//     console.log(`this ${meroCar.name} is my car with model no ${meroCar.model}`)
+//     console.log(`this ${meroCar.name} is my car with model no ${meroCar.model}`)*/
 
 
 
-//func   fname   constructors
+/*
+///func   fname   constructors
   function Car(model, name, price){ //constructor declaration
     this.carModel = model;
     this.carName = name;
@@ -76,31 +77,82 @@ console.log(me.fullName())
 const newCar = new Car (2000, "toyota", 4000000);
 
 console.log(`this is my car with model no ${newCar.carModel}, name ${newCar.carName}, and it costs ${newCar.carPrice} `)
-//output : this is my car with model no 2000, name toyota, and it costs 4000000
+///output : this is my car with model no 2000, name toyota, and it costs 4000000
 
 
 
-// to add new we use prototype
+/// to add new we use prototype
 Car.prototype.color = "black";
 console.log(`this is my car with model no ${newCar.carModel}, name ${newCar.carName}, 
     and it costs ${newCar.carPrice}, looks bold in ${newCar.color}`)
-    //output : this is my car with model no 2000, name toyota, and it costs 4000000, looks bold in black
+    ///output : this is my car with model no 2000, name toyota, and it costs 4000000, looks bold in black
 
     function Person(first, last, age, eyecolor) {
   this.firstName = first;
   this.lastName = last;
   this.age = age;
   this.eyeColor = eyecolor;
+  
 }
 
 const myMother = new Person("Sally", "Rally", 48, "green");
 
 
-//methods of object
+///methods of object
 const myFather = {firstName: "John", lastName: "Doe", age: 50 };// 
 console.log(`my mom name is ${myMother.firstName} ${myMother.lastName}`)
  Object.assign(Person, myFather)
 let text = Object.entries(Person)
-console.log(typeof text)
+console.log(typeof text)*/
 
+
+/*//GET FUNC keywords
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  language: "en",
+  get lang() {
+    return this.language;
+  }
+};
+console.log(`this is my language ${person.lang}`)
+
+
+//Set Function
+const humans  = {
+  color : "white",
+  age : 21, 
+  fullName:"" , 
+  class: "",
+set myclass(myclass){
+  this.class = myclass;
+}
+
+// set name(name){
+//   this.fullName = name;
+// }
+};
+humans.class = 12
+console.log(humans.class)
+// humans.name = "Niranjan"; 
+
+// console.log(humans.fullName)
+
+//both func
+const human = {
+  color : "white",
+  age : 21, 
+  address : "nepal",
+  fullName : "",
+  
+  // get add(){         //this is get
+  //   return this.address;
+  // }
+  // set myname(myname){     //this is set
+  //   this.fullName = myname
+  // }
+};
+// console.log("this is my ad " + human.add)  //for get
+// human.myname = "Niranjan Katwal"           //for set
+// console.log(`hi its me ${human.fullName}`) */
 
