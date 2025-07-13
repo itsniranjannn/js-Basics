8d0fd5bb7518dd90f8f2a764da2176862f75f421
itsniranjannn <niranjankatwal33@gmail.com>
2025-07-13 21:06:41 +0545

    super Keyword in class

--- objclass.js
+++ objclass.js
@@ -69,35 +69,111 @@
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~ classes ~~~~~~~~~~~~~~~~~~~~~~~~~~
-class myCar{   //this is a class that acts as a templete to create an object
-    constructor(brand, price){  //here brand and price are (param) or propertise  of constructor
-        this.myBrand = brand
-        this.myPrice = price 
-    }
-}
+// class myCar{   //this is a class that acts as a templete to create an object
+//     constructor(brand, price){  //here brand and price are (param) or propertise  of constructor
+//         this.myBrand = brand
+//         this.myPrice = price 
+//     }
+// }
 
-const tata = new myCar ('toyota', 2000000)
-console.log(`tata brand is ${tata.myBrand}, and it costs ${tata.myPrice}`)//tata brand is toyota, and it costs 2000000
+// const tata = new myCar ('toyota', 2000000)
+// console.log(`tata brand is ${tata.myBrand}, and it costs ${tata.myPrice}`)//tata brand is toyota, and it costs 2000000
 
 
 //________________inheritance in classes-----------------------
-class goodstudent {
-    read(){
-        console.log(`reading books`);
-        }
-    write(){
-        console.log(`writes notes`);
+// class goodstudent {
+//     read(){
+//         console.log(`reading books`);
+//         }
+//     write(){
+//         console.log(`writes notes`);
         
-    }
-}
-const ram = new goodstudent();  // if run ram.write() = writes notes
+//     }
+// }
+// const ram = new goodstudent();  // if run ram.write() = writes notes
 
-class badstudent extends goodstudent{  //here badstudents inherits from good student   
-    //it has access to read and write fuction of good student
-    activity(){
-        console.log(`does bully, disobeident`);
+// class badstudent extends goodstudent{  //here badstudents inherits from good student //it has access to read and write fuction of good student
+//     activity(){
+//         console.log(`does bully, disobeident`);
         
-    }
-}
+//     }
+// }
 
-const ravan = new badstudent();   // if run ravan.write() = writes notes   // ravan.activity() = does bully, disobeident
+// const ravan = new badstudent();   // if run ravan.write() = writes notes   // ravan.activity() = does bully, disobeident
+
+
+//+++++++++++++++++ METHOD OVERRIDING ++++++++++++++++++
+// if child class and parent class have same method , child method will be used   its a method overriding
+// class goodstudent {
+//        constructor(section, rollno) {   //we passed A param or propertise also
+//         this.class = section
+//         this.roll = rollno
+//     }
+  
+//    read(){
+//         console.log(`reading books`);
+//         }
+//     write(){
+//         console.log(`writes notes`);   
+//     }
+//     activity (){
+//         console.log(`obedient, calm and polite`);       
+//     }
+// }
+// const ram = new goodstudent('7', 11)
+// console.log(`this is goodstudents class ${ram.class}, and roll no is ${ram.roll}`)
+
+// class badstudent extends goodstudent{  //here badstudents inherits from good student, it has access to fuction of good student
+//     activity(){
+//         console.log(`does bully, disobeident`);
+//     }
+// }
+// const ravan = new badstudent(6, 56); 
+// ravan.activity() //does bully, disobeident  //here activity() is same in good and badstudent since badstudent is child , its method is user
+// ravan.read() //reading books
+
+// console.log(`this is badstudent class ${ravan.class}, and roll no is ${ravan.roll}.`)
+
+
+
+//----------------Super Keyword in INHERITANCE-----------------
+
+// super keyword helps to access the parent method via super.parentmethod(), and to call parent constructor via super(params)
+// class fruits {
+//     constructor(name, price){
+//         this.fname = name
+//         this.fprice = price
+//     }
+//     sweet(){
+// console.log(`b loody Sweeeeeeet`)
+//     }
+// }
+
+// class choco extends fruits{
+//      taste(){
+//         super.sweet()  //this helps to access the parents method in child = output 1st b loody Sweeeeeeet then sugar crush
+//         console.log(`sugar crush`);
+//      }
+// }
+
+// const dairymilk = new choco('dairymilk', 1000)
+// console.log(dairymilk.fprice)
+
+
+
+//to invoke param of parent constructor
+
+// class sub{
+//     constructor(sname){
+//      this.subName = sname
+//     }
+// }
+// class fabsub extends sub{
+//     constructor(sname){
+//         super(sname) // this helps to pass this sname of this class to parent class
+//     }
+// }
+
+// const favsubj = new fabsub ('Science')
+
+// console.log(favsubj.subName)
