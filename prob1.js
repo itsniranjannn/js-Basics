01a7e09137a2b011404e1bc28bce3edad7e791a0
itsniranjannn <niranjankatwal33@gmail.com>
2025-07-13 21:56:46 +0545

    2nd qn solution

--- prob1.js
+++ prob1.js
@@ -15,3 +15,32 @@
 // const ceo = new user ('CEO', 'ceo@gmail.com')
 // console.log(ceo.viewData())     //this is user CEO, and useremail is ceo@gmail.com
 
+
+
+// Qn 2 in above create an admin that inherits from user and add new method edit data to admin
+let data = 'top secret'
+class user {
+    constructor(name, email){
+    this.uName = name
+    this.uEmail = email
+    }
+    viewData(){
+        console.log(`Data = ${data}`)  //Data = top secret
+        console.log(`this is user ${this.uName}, and useremail is ${this.uEmail}`)//this is user admin, and useremail is ad@gmail.com
+    }
+}
+
+class admin extends user {
+    
+    editdata(){
+        super.viewData()
+        data = 'change is made in data and is encrypted'
+        
+        console.log(`you are admin and allowed to edit the datas`)//you are admin and allowed to edit the datas
+        console.log(`Edited Data : ${data} `)//Edited Data : change is made in data and is encrypt
+    }
+}
+
+const admin1 = new admin ('admin', 'ad@gmail.com')
+
+console.log(admin1.editdata())
