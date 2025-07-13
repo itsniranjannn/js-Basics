b38a2e4498166ee44de66400caaabebf8a5222fa
itsniranjannn <niranjankatwal33@gmail.com>
2025-07-13 21:35:24 +0545

    qn 1 regarding class

---
+++ prob1.js
@@ -0,0 +1,17 @@
+//qn 1 is to create user with 2 proopertise , and have viewdata method
+
+// class user {
+//     constructor(name, email){
+//     this.uName = name
+//     this.uEmail = email
+//     }
+//     viewData(){
+//         console.log(`this is user ${this.uName}, and useremail is ${this.uEmail}`)
+//     }
+// }
+// const user1 = new user('Niranjan', 'nk@gmail.com')  
+// console.log(user1.viewData())  //this is user Niranjan, and useremail is nk@gmail.com
+
+// const ceo = new user ('CEO', 'ceo@gmail.com')
+// console.log(ceo.viewData())     //this is user CEO, and useremail is ceo@gmail.com
+
