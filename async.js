69a9197058753446dbb11b19421d7d915256c61d
itsniranjannn <niranjankatwal33@gmail.com>
2025-07-14 13:36:18 +0545

    callback hell || pyramid of doom

--- async.js
+++ async.js
@@ -9,7 +9,7 @@
 // console.log(`i am second`);
 // console.log(`i am 3rd`);
 
-
+//============CallBAck Hell || Pyramid Of Doom==================
 // function user (userid){
 //     setTimeout(()=>{
 //         console.log('Id = ', userid)
@@ -19,28 +19,27 @@
 // user (2)
 
 // but in practical scenario we may want to get the user 1 first and user 2 after a 2s gap 
-function user (userid, getNextuser){
-    setTimeout(()=>{
-        console.log('Id = ', userid)
+// function user (userid, getNextuser){
+//     setTimeout(()=>{
+//         console.log('Id = ', userid);
 
-        if(getNextuser){   //this means if getNextuser exists then execute below function 
-            getNextuser()
-        }
-    }, 3000)
-}
-// here now we have placed A  getNextuser as a parameter which we will pass  
-user(1, () => {    //here arrow function is getnextuser which executes now after 3 sec of user 1
-    console.log('getting id of next user ')
-    user(2 , ()=>{
-        console.log(`getting id of next user`);
+//         if(getNextuser){   //this means if getNextuser exists then execute below function 
+//             getNextuser()
+//         }
+//     }, 3000);
+// }// here now we have placed A  getNextuser as a parameter which we will pass  
+// user(1, () => {    //here arrow function is getnextuser which executes now after 3 sec of user 1
+//     console.log('getting id of next user ');
+//     user(2 , ()=>{
+//         console.log(`getting id of next user`);
         
-        user(3)
-    })
-})
+//         user(3)
+//     });
+// });
 /* this callback inside call back is called CALLBACK HELL
 Nestead  callback in a stack one inside another is  called Callback hell || Pyramid of doom
 user(1, () => {    
     user(2 , ()=>{
         user(3)
-    })
+    });
 })*/
