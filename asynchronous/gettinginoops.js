d8644f6c60b8296e05b7d68f1bcd06b568c5207b
itsniranjannn <niranjankatwal33@gmail.com>
2025-07-19 16:25:42 +0545

    class, with func and prototype

--- asynchronous/gettinginoops.js
+++ asynchronous/gettinginoops.js
@@ -48,16 +48,38 @@
 
 
 //                Call                   //
-function setname (name){
-    this.name = name
-}
+// function setname (name){
+//     this.name = name
+// }
 
-function user(name, address, id ){
-   setname.call(this, name)   //when i use call to hold the refrence there all the execution context are removed of above function
+// function user(name, address, id ){
+//    setname.call(this, name)   //when i use call to hold the refrence there all the execution context are removed of above function
 //    here i have placed this cause when all context are removed this of abobe have nothing so i gave this to refer this function context
-    this.address = address
-    this.id = id
-}
+//     this.address = address
+//     this.id = id
+// }
 
-let user1 = new user('nirajan', 'kathmandu', 20)
-console.log(user1) //user { name: 'nirajan', address: 'kathmandu', id: 20 }
+// let user1 = new user('nirajan', 'kathmandu', 20)
+// console.log(user1) //user { name: 'nirajan', address: 'kathmandu', id: 20 }
+
+
+//==============  Class ==============//
+// class login {
+//     constructor(username, email, password){
+//         this.username = username
+//         this.email = email
+//         this.password = password
+//     }
+//     newUserName(){   // created a function to chnage username
+//         return`Mr ${this.username}`
+//     }
+// }
+
+// login.prototype.encrypt = function(){  //created fuction using prototype
+//     console.log(`you pass word is encrypted : a2${this.password}3b4`)
+// }
+
+// const newUser = new login ('niranjan', 'n@gmail.com', '981676')
+
+// newUser.encrypt()  //you pass word is encrypted : a29816763b4
+// console.log(newUser.newUserName()) //Mr niranjan
