ed34a2b4b4a709c8a17c01d7821da4acfec605db
itsniranjannn <niranjankatwal33@gmail.com>
2025-07-19 12:59:44 +0545

    call and us e of this

--- asynchronous/gettinginoops.js
+++ asynchronous/gettinginoops.js
@@ -19,29 +19,45 @@
 
 
 
-function kheladi (player , sport, age){
-    this.player = player
-    this.sport = sport
-    this.age = age
+// function kheladi (player , sport, age){
+//     this.player = player
+//     this.sport = sport
+//     this.age = age
+// }
+// const player1 = new kheladi ('NeyMar Jr', 'football', 28)
+
+// const player2 = new kheladi ('Bhurtel', 'cricket', 28)
+
+// const player = new kheladi ("Sandeep lamichane", 'cricket', 27)
+
+// kheladi.prototype.printme= function(){   // created a prototype function this can be used by any object now
+//     console.log(`this is  kheladi no 420 ${this.player}`)
+// }
+// player1.printme()  // this is  kheladi no 420 NeyMar Jr
+// player2.printme()  //this is  kheladi no 420 Bhurtel
+// player.printme()  //this is  kheladi no 420 Sandeep lamichane
+
+
+
+// kheladi.prototype.length = function(){   // created a function for calculating lengeth of string
+//     console.log(`length is ${this.player.trim().length}`)
+// }
+// player1.length()//length is 9   
+// player2.length()//length is 7
+// player.length()//length is 17
+
+
+//                Call                   //
+function setname (name){
+    this.name = name
 }
-const player1 = new kheladi ('NeyMar Jr', 'football', 28)
 
-const player2 = new kheladi ('Bhurtel', 'cricket', 28)
-
-const player = new kheladi ("Sandeep lamichane", 'cricket', 27)
-
-kheladi.prototype.printme= function(){   // created a prototype function this can be used by any object now
-    console.log(`this is  kheladi no 420 ${this.player}`)
+function user(name, address, id ){
+   setname.call(this, name)   //when i use call to hold the refrence there all the execution context are removed of above function
+//    here i have placed this cause when all context are removed this of abobe have nothing so i gave this to refer this function context
+    this.address = address
+    this.id = id
 }
-player1.printme()  // this is  kheladi no 420 NeyMar Jr
-player2.printme()  //this is  kheladi no 420 Bhurtel
-player.printme()  //this is  kheladi no 420 Sandeep lamichane
 
-
-
-kheladi.prototype.length = function(){   // created a function for calculating lengeth of string
-    console.log(`length is ${this.player.trim().length}`)
-}
-player1.length()//length is 9   
-player2.length()//length is 7
-player.length()//length is 17
+let user1 = new user('nirajan', 'kathmandu', 20)
+console.log(user1) //user { name: 'nirajan', address: 'kathmandu', id: 20 }
