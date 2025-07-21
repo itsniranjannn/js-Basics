da955f47561b5debf5667b27a29a31f714ec355b
itsniranjannn <niranjankatwal33@gmail.com>
2025-07-21 11:06:08 +0545

    new about array

---
+++ asynchronous/newABOUTarray.js
@@ -0,0 +1,24 @@
+//   ====   Somethiing new About Array   ====   //
+//Arraya are either  - continious (packed)    - Holeyyyy
+
+//SMI = Small Integer
+//Packed Integr
+//Double Integer
+
+
+const array = [1, 2, 3, 4, 5]
+//This is Packed Smi
+
+array.push(6.0)
+// console.log(array)  //[ 1, 2, 3, 4, 5, 6 ] Now this is Packed Double Integer
+
+array.push('7')
+// console.log(array);  //[ 1, 2, 3, 4, 5, 6, '7']  this is Packed integer
+
+// Once a array is change it cant be reversed like if SMI is change to Double Integer It cant be reversed to SMI
+
+
+
+
+
+
