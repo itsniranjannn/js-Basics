c028a22844190349ef27373a06f688f556a366ff
itsniranjannn <niranjankatwal33@gmail.com>
2025-07-22 11:50:26 +0545

    about cookie

---
+++ asynchronous/cookies.js
@@ -0,0 +1,6 @@
+//~~~~~~~~~~~~~~~~~ Cookies in Javascript ~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+// Cookies are small pieces of data stored on a user's device by a web browser.
+//  They are used to store information about the user's interactions with a website, such as login credentials, user preferences, and shopping cart contents.
+//  Cookies are created by a web server and sent to the user's browser as part of the HTTP response headers
+
