0c94405fd9fe78eacaa3564b626a2f9c602a6902
itsniranjannn <niranjankatwal33@gmail.com>
2025-07-22 11:50:46 +0545

    cookie

--- asynchronous/cookies.js
+++ asynchronous/cookies.js
@@ -4,3 +4,9 @@
 //  They are used to store information about the user's interactions with a website, such as login credentials, user preferences, and shopping cart contents.
 //  Cookies are created by a web server and sent to the user's browser as part of the HTTP response headers
 
+// document.cookie is used to create a cookie in js
+// cookies are in name = value pair
+
+// document.cookie = "username=John Doe; expires=Thu, 18 Dec 2023 12:00:00 UTC; path=/";   
+
+// to delete cookie set date to past date
